b9012528f4dd53a0c4f0c2029359121948d45b6d
onyxgeek <onyxpeek@gmail.com>
2023-11-05 12:25:52 +0200

    fix: change my class to bogdan one

--- src/BogdanClasses/SoundBar.java
+++ src/BogdanClasses/SoundBar.java
@@ -1,5 +1,73 @@
 package BogdanClasses;
 
-public class SoundBar {
 
+
+public class SoundBar {
+    private int volume;
+    private boolean isOn;
+
+    public SoundBar() {
+        this.volume = 50; // setează volumul inițial la 50
+        this.isOn = false; // soundbar-ul este oprit când este creat
+    }
+
+    // Constructor cu parametri
+    public SoundBar(int initialVolume, boolean initialOnState) {
+        if (initialVolume >= 0 && initialVolume <= 100) {
+            this.volume = initialVolume;
+        } else {
+            System.out.println("Volumul trebuie să fie între 0 și 100. Setat la 50 implicit.");
+            this.volume = 50;
+        }
+
+        this.isOn = initialOnState;
+    }
+
+    public void powerOn() {
+        isOn = true;
+        System.out.println("SoundBar: Pornit");
+    }
+
+    public void powerOff() {
+        isOn = false;
+        System.out.println("SoundBar: Oprit");
+    }
+
+    public void setVolume(int volume) {
+        if (isOn) {
+            if (volume >= 0 && volume <= 100) {
+                this.volume = volume;
+                System.out.println("SoundBar: Volumul setat la " + volume);
+            } else {
+                System.out.println("SoundBar: Volumul trebuie să fie între 0 și 100");
+            }
+        } else {
+            System.out.println("SoundBar: Nu poți seta volumul când soundbar-ul este oprit");
+        }
+    }
+
+    public void playAudio(String audio) {
+        if (isOn) {
+            System.out.println("SoundBar: Redare audio - " + audio);
+        } else {
+            System.out.println("SoundBar: Soundbar-ul este oprit. Nu se poate reda audio.");
+        }
+    }
+
+    public void mute() {
+        if (isOn) {
+            this.volume = 0;
+            System.out.println("SoundBar: Sunetul este mutat");
+        } else {
+            System.out.println("SoundBar: Nu poți opri sunetul când soundbar-ul este oprit");
+        }
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public boolean isOn() {
+        return isOn;
+    }
 }
--- src/HomeEntertainmentSystem.java
+++ src/HomeEntertainmentSystem.java
@@ -1,21 +1,8 @@
 //Importarea claselor
-import NicuClasses.SoundBar;
+import BogdanClasses.SoundBar;
 
 public class HomeEntertainmentSystem {
     public static void main(String[] args) {
-        // Creaza un obiect SoundBar folosind constructorul implicit
-        SoundBar soundBarImplicit = new SoundBar();
 
-        // Porneste soundbar-ul
-        soundBarImplicit.powerOn();
-
-        // Seteaza volumul
-        soundBarImplicit.setVolume(70);
-
-        // Redă un audio
-        soundBarImplicit.playAudio("Melodie preferată");
-
-        // Opreșe soundbar-ul
-        soundBarImplicit.powerOff();
     }
 }
