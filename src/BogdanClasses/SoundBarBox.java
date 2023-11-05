4f3a6cb6b619494783aa945d2ae6272d978fc3a0
usk231 <acsbogdan2002@yahoo.com>
2023-11-05 03:25:05 -0800

    done:methods for SoundBarBox Class

--- src/BogdanClasses/SoundBarBox.java
+++ src/BogdanClasses/SoundBarBox.java
@@ -2,12 +2,12 @@ package BogdanClasses;
 
 public class SoundBarBox {
     private int Volume;
-    private int BassLevel=30;
+    private int Bass=30;
     private boolean isPoweredOn=false;
-    private int balance=50;
+    private int Balance=50;
 
     public SoundBarBox() {
-        this.volume = 10; // setez volumul initial la 10
+        this.Volume = 10; // setez volumul initial la 10
         this.isPoweredOn = false; // soundbar-ul este oprit atunci cand este creat
     }
 
@@ -23,7 +23,7 @@ public class SoundBarBox {
         System.out.println("SoundBar-ul a fost oprit!");
     }
 
-    //Metoda pentru a adjusta volumul soundbar-ului//
+    //Metoda pentru a ajusta volumul soundbar-ului//
 public void AdjustVolume(int Volume){
         if(Volume>=0&&Volume<=100) {
             this.Volume = Volume;
@@ -33,28 +33,65 @@ public void AdjustVolume(int Volume){
             System.out.println("Volumul soundbar-ului trebuie sa fie intre 0 si 100.");
         }
 }
+
+//Metoda pentru a ajusta nivelul bass-ului//
+    public void AdjustBass(int Bass){
+        if(Bass>=0&&Bass<=100) {
+            this.Bass = Bass;
+            System.out.println("Volumul bass-ului a fost setat la:" + Bass + ".");
+        }
+        else {
+            System.out.println("Volumul bass-ului trebuie sa fie intre 0 si 100.");
+        }
+    }
+
+//Metoda pentru a ajusta echilibrul//
+public void AdjustBalance(int Balance){
+    if(Balance>=0&&Balance<=100) {
+        this.Balance = Balance;
+        System.out.println("Balanta a fost setata la:" + Bass + ".");
+    }
+    else {
+        System.out.println("Balanta trebuie sa fie intre 0 si 100.");
+    }
+}
+
+//Metoda pentru redarea audio//
     public void playAudio(String audio) {
-        if (isOn) {
+        if (isPoweredOn) {
             System.out.println("SoundBar: Redare audio - " + audio);
         } else {
             System.out.println("SoundBar: Soundbar-ul este oprit. Nu se poate reda audio.");
         }
     }
 
+//Metoda pentru mute//
     public void mute() {
-        if (isOn) {
-            this.volume = 0;
-            System.out.println("SoundBar: Sunetul este mutat");
+        if (isPoweredOn) {
+            this.Volume = 0;
+            System.out.println("SoundBar: Sunetul este oprit.");
         } else {
-            System.out.println("SoundBar: Nu poți opri sunetul când soundbar-ul este oprit");
+            System.out.println("SoundBar: Nu poți opri sunetul când soundbar-ul este oprit.");
         }
     }
 
+    //Metoda pentru a obtine volumul curent//
     public int getVolume() {
-        return volume;
+        return Volume;
     }
 
-    public boolean isOn() {
-        return isOn;
+    //Metoda pentru a verifica daca soundbar-ul este pornit/oprit//
+    public boolean isPoweredOn() {
+        return isPoweredOn;
+    }
+
+    //Metoda pentru a obtine echilibrul//
+    public int getBalance() {
+        return Balance;
+    }
+
+    //Metoda pentru a obtine bass-ul//
+    public int getBass() {
+        return Bass;
     }
 }
